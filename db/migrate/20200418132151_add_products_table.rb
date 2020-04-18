868814c572e4233026b914ad5243d7fcd995818b
Punkbooster <ugliar.arsen@gmail.com>
2020-04-18 15:30:23 +0200

    Add products migration

---
+++ db/migrate/20200418132151_add_products_table.rb
@@ -0,0 +1,15 @@
+class AddProductsTable < ActiveRecord::Migration[5.2]
+  def up
+    create_table :products do |t|
+      t.string :name, index: true
+      t.text :description
+      t.decimal :price, precision: 8, scale: 2, null: false
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :products
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_124455) do
+ActiveRecord::Schema.define(version: 2020_04_18_132151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_04_18_124455) do
     t.string "website_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_products_on_name"
   end
 
 end
