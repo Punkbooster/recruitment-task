6d072c6a644a44cf265641d81febe52acc91221c
Punkbooster <ugliar.arsen@gmail.com>
2020-04-20 19:35:41 +0200

    Refactor normalize query service

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,5 @@
 class Label < ApplicationRecord
-  has_many :products, inverse_of: :label, dependant: :destroy
+  has_many :products, inverse_of: :label, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
--- app/services/normalize_query_service.rb
+++ app/services/normalize_query_service.rb
@@ -5,22 +5,18 @@ class NormalizeQueryService
 
   def call
     normalize_query
-
-    @result
   end
 
   private
 
   def normalize_query
-    @result = []
-
-    @query.split(' ').each do |query_element|
+    @query.split(' ').map do |query_element|
       if query_element.include?(':')
         association_name, value = query_element.split(':')
 
-        @result << { association_name: association_name.to_sym, query_value: value }
+        { association_name: association_name.to_sym, query_value: value }
       else
-        @result << { association_name: :product, query_value: query_element }
+        { association_name: :product, query_value: query_element }
       end
     end
   end
